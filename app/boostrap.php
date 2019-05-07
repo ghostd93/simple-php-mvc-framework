15276a4064fae334a1865e0467d902c47d2e8de6
Jakub Pawluk <p.kuba1993@gmail.com>
2019-05-07 11:50:48 +0200

    create config file

--- app/boostrap.php
+++ app/boostrap.php
@@ -1,4 +1,8 @@
 <?php
+// load config
+require_once 'config/config.php';
+
+// load libs
 require_once 'libraries/Core.php';
 require_once 'libraries/Database.php';
 require_once 'libraries/Controller.php';
---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'http://localhost/php-mvc-framework');
+
+// App Name
+define('APP_NAME', 'PHP-MVC-FRAMEWORK');
