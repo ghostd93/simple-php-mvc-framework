5c83afd9d035912648be02da79217cd2d49d10cd
Jakub Pawluk <p.kuba1993@gmail.com>
2019-05-07 11:54:46 +0200

    autoload core libs

--- app/boostrap.php
+++ app/boostrap.php
@@ -2,7 +2,7 @@
 // load config
 require_once 'config/config.php';
 
-// load libs
-require_once 'libraries/Core.php';
-require_once 'libraries/Database.php';
-require_once 'libraries/Controller.php';
+// autoload core libs
+spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+});
