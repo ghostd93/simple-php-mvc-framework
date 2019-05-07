3007fae18630d6b61bf561c402674a32984b4a78
Jakub Pawluk <p.kuba1993@gmail.com>
2019-05-07 13:17:19 +0200

    database class completed

--- app/libraries/database.php
+++ app/libraries/database.php
@@ -33,5 +33,47 @@ class Database {
 
     }
 
+    // prepare query
+    public function query($sql){
 
+        $this->statement = $this->handler->prepare($sql);
+
+    }
+
+    // bind parameters
+    public function bind($param, $value, $type = null){
+
+        if(is_null($type)){
+            switch($type){
+                case is_int($value): $type = PDO::PARAM_INT; break;
+                case is_bool($value): $type = PDO::PARAM_BOOL; break;
+                case is_null($value): $type = PDO::PARAM_NULL; break;
+                default: $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->statement->bindValue($param, $value, $type);
+    }
+
+    // execute statement
+    public function execute(){
+        return $this->statement->execute();
+    }
+
+    // get result set
+    public function resultSet(){
+        $this->execute();
+        return $this->statement->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    // get single row
+    public function single(){
+        $this->execute();
+        return $this->statement->fetch(PDO::FETCH_OBJ);
+    }
+
+    // row count
+    public function rowCount(){
+        return $this->statement->rowCount();
+    }
 }
--- app/mvc/controller/PagesController.php
+++ app/mvc/controller/PagesController.php
@@ -3,11 +3,15 @@
 class PagesController extends Controller{
 
     public function __construct(){
-        $this->testModel = $this->model('Test');
     }
 
     public function index(){
-        $this->view('pages/index', ['title' => 'Welcome']);
+
+        $data = [
+            'title' => 'Welcome',
+        ];
+
+        $this->view('pages/index', $data);
     }
 
     public function about(){
