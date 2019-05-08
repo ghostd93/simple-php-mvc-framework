3c4c38625793dba541c72fa8565b39f41d48f90b
Jakub Pawluk <p.kuba1993@gmail.com>
2019-05-08 16:06:22 +0200

    Database - binding parameters fixed

--- app/libraries/database.php
+++ app/libraries/database.php
@@ -44,7 +44,7 @@ class Database {
     public function bind($param, $value, $type = null){
 
         if(is_null($type)){
-            switch($type){
+            switch(true){
                 case is_int($value): $type = PDO::PARAM_INT; break;
                 case is_bool($value): $type = PDO::PARAM_BOOL; break;
                 case is_null($value): $type = PDO::PARAM_NULL; break;
